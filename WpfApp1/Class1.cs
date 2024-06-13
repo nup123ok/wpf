ce8b971d2dd9dc95eaa52fe81af70095db486a47
nup123ok <nup123ok@gmail.com>
2024-06-13 23:42:20 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    class Class1
+    {
+        public static Frame mainFrame { get; set; }
+    }
+}
